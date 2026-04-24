8b48c131e0b1f03dfd1af2742f63daec90a9167b
SagarTPJ <mssmriti000@gmail.com>
2026-04-24 11:23:37 +0545

    Update device.mk to change shipping API level
    Removed inheritance of virtual A/B and updated shipping API level to 34.

--- device.mk
+++ device.mk
@@ -1,10 +1,8 @@
-# Inherit Virtual A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
-# Set shipping level to 35 (Android 15)
+# Compatibility level
 PRODUCT_SHIPPING_API_LEVEL := 34
 
-# Fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.1-impl-mock \
     fastbootd
