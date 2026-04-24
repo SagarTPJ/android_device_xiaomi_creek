b0ebe6f349fbae9722e18229685201aa64de4b66
SagarTPJ <mssmriti000@gmail.com>
2026-04-24 17:57:55 +0545

    Downgrade PRODUCT_SHIPPING_API_LEVEL to 32

--- device.mk
+++ device.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # Compatibility level
-PRODUCT_SHIPPING_API_LEVEL := 34
+PRODUCT_SHIPPING_API_LEVEL := 32
 
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.1-impl-mock \
