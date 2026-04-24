55174c7896836344c63bf11075b7b0ea78562264
SagarTPJ <mssmriti000@gmail.com>
2026-04-24 08:07:27 +0545

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,39 +1,10 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
+# Virtual A/B
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
-LOCAL_PATH := device/xiaomi/creek
+# Shipping API
+PRODUCT_SHIPPING_API_LEVEL := 34
 
-# Inherit from TWRP common config (Fixes the 'vendor/omni' error)
-$(call inherit-product, vendor/twrp/config/common.mk)
-
-# A/B - Postinstall configuration
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=erofs \
-    POSTINSTALL_OPTIONAL_system=true
-
-# Boot Control HAL
+# Fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl \
-    android.hardware.boot@1.2-service \
-    android.hardware.boot@1.2-service.recovery
-
-PRODUCT_PACKAGES += \
-    bootctrl.creek
-
-# Update Engine and Sideloading
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh \
-    update_engine \
-    update_verifier \
-    update_engine_sideload \
-    checkpoint_gc
-
-# Dynamic Partitions
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
+    android.hardware.fastboot@1.1-impl-mock \
+    fastbootd
