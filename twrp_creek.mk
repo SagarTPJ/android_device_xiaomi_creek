0832b695601b88ce9ac67dc3cf21e6aba5f8a497
SagarTPJ <mssmriti000@gmail.com>
2026-04-24 12:33:36 +0545

    Add TWRP configuration for POCO M7 device

---
+++ twrp_creek.mk
@@ -0,0 +1,16 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from the device-specific configuration
+$(call inherit-product, device/xiaomi/creek/device.mk)
+
+# Inherit TWRP common bits
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device identifier
+PRODUCT_DEVICE := creek
+PRODUCT_NAME := twrp_creek
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := POCO M7
+PRODUCT_MANUFACTURER := xiaomi
