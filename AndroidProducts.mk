c7b5705e7eabb23a9fefc3ed0a2f911dc78c8bb0
SagarTPJ <mssmriti000@gmail.com>
2026-04-24 07:44:06 +0545

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,5 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_creek.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_creek-user \
-    twrp_creek-userdebug \
     twrp_creek-eng
