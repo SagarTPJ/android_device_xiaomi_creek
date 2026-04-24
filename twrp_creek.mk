3cdcddfff2efa2889958f9f36598bc44c25b16ee
SagarTPJ <mssmriti000@gmail.com>
2026-04-24 08:57:31 +0545

    Clean up twrp_creek.mk by removing comments
    Removed unnecessary comments and a GMS client ID.

--- twrp_creek.mk
+++ twrp_creek.mk
@@ -1,18 +1,10 @@
-# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit from creek device
 $(call inherit-product, device/xiaomi/creek/device.mk)
-
-# Inherit TWRP common bits
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Device identifier
 PRODUCT_DEVICE := creek
 PRODUCT_NAME := twrp_creek
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO M7
 PRODUCT_MANUFACTURER := xiaomi
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
